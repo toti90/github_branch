4b2dd3f7992c3cd3cb068054f18df84958aaaf73
Tamás Tóth <tothtomi7@gmail.com>
2022-05-05 18:35:27 +0200

    Task 1 name (#1)
    * add console log
    
    * add new function
    
    * remove console log

--- server.ts
+++ server.ts
@@ -2,3 +2,8 @@ function addTwoNumber(num1: number, num2: number): number {
   let result = num1 + num2;
   return result;
 }
+
+function multiplyNumbers(num1: number, num2: number): number {
+  let result = num1 * num2;
+  return result;
+}
