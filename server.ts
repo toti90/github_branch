f14d7b65e2b526bb6ed5a1129f54cbe8dd931b62
Tamás Tóth <tothtomi7@gmail.com>
2022-05-05 18:52:17 +0200

    refactor done (#3)

--- server.ts
+++ server.ts
@@ -2,6 +2,6 @@ function addTwoNumber(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-function multiplyNumbers(num1: number, num2: number): number {
-  return num1 * num2;
+function multiplyNumbers(numbers: number[]): number {
+  return numbers.reduce((num1, num2) => num1 + num2);
 }
