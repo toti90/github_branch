b0e09919246f7934601973bb3723c42c94bc01e3
toti90 <tothtomi7@gmail.com>
2022-05-05 18:31:30 +0200

    add new function

--- server.ts
+++ server.ts
@@ -3,3 +3,8 @@ function addTwoNumber(num1: number, num2: number): number {
   console.log(result);
   return result;
 }
+
+function multiplyNumbers(num1: number, num2: number): number {
+  let result = num1 * num2;
+  return result;
+}
